57d2c742d78189377d5bda74d9475ae616a3479d
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-29 17:58:02 +0000

    Add NameLine component

--- src/ui/components/Player/index.tsx
+++ src/ui/components/Player/index.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.css";
 import classNames from "classnames/bind";
 
