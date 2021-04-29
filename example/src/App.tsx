ee636013b0b045cd827210220c0a15333af90e83
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-29 21:17:47 +0300

    Merge branch 'master' of git.name-coach.com:shushpan/gpdb-widget into recorder-slider

---
+++ example/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Widget, loadClient } from 'gpdb-components'
+
+const App = () => {
+  const applicationContext = { instanceSig: 'name-coach.com', typeSig: 'email_dns_name' }
+  const nameOwnerContext = { signature: 'jon.snow@name-coach.com' }
+  const userContext = { email: 'jon.snow@name-coach.com', signature: 'jon.snow@name-coach.com' }
+
+  const client = loadClient(
+    {
+      accessKeyId: process.env.REACT_APP_GPDB_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_GPDB_SECRET_ACCESS_KEY
+    },
+    applicationContext,
+    nameOwnerContext,
+    userContext
+  )
+
+  return <Widget client={client} name="Jack Snow" width={600} />
+}
+
+export default App
