7d43ce0a7469c2aa4f6ec2b3d0ea65a780452e33
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-30 01:42:01 +0300

    Updates

---
+++ precache-manifest.0d5fd422fa8f22c721205757916a1a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7042739b901e17cf28ad863842302948",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a9c686a46202af99fed",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "26aad853d4f2329ff66f",
+    "url": "./static/js/2.7fbe1128.chunk.js"
+  },
+  {
+    "revision": "1d0240a6e719653d3725cfae4cd3222b",
+    "url": "./static/js/2.7fbe1128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9c686a46202af99fed",
+    "url": "./static/js/main.98439a33.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "./static/js/main.98439a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3196c11dae49ff7f3032",
+    "url": "./static/js/runtime-main.bf8f3435.js"
+  }
+]);
