7267a98b1c872a44afb8b1232fec2540f10fd1d9
NameCoachDevTeam <dev-team@name-coach.com>
2021-04-30 00:50:05 +0300

    chore(release): 1.0.1

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { Widget, loadClient } from 'gpdb-components'
+import { Widget, loadClient } from 'gpdb-widget'
+import { useDebouncedCallback } from 'use-debounce'
+
+const style = {
+  margin: '50px auto 0 auto',
+}
 
 const App = () => {
+  const [name, setName] = useState('Jon Snow');
+
   const applicationContext = { instanceSig: 'name-coach.com', typeSig: 'email_dns_name' }
   const nameOwnerContext = { signature: 'jon.snow@name-coach.com' }
   const userContext = { email: 'jon.snow@name-coach.com', signature: 'jon.snow@name-coach.com' }
@@ -17,7 +24,28 @@ const App = () => {
     userContext
   )
 
-  return <Widget client={client} name="Jack Snow" width={600} />
+  const debounced = useDebouncedCallback(
+    (value) => {
+      if (value.trim().length === 0) return;
+      setName(value);
+    },
+    600
+  );
+
+  return <>
+    <div style={{ margin: "50px auto 0 auto", width: "400px" }}>
+      Name:
+      <input
+        defaultValue={name}
+        type="text"
+        onChange={(e) => debounced(e.target.value)}
+        required
+        style={{ width: "80%", marginLeft: "20px" }}
+      />
+    </div>
+
+    <Widget client={client} name={name} width={600} style={style} />
+  </>
 }
 
 export default App
