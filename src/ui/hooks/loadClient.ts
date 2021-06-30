e22077c612cf3b24ff53283dcdd08a84f6b6c54c
NameCoachDevTeam <dev-team@name-coach.com>
2021-06-30 19:25:26 +0300

    Remove unneeded headers

--- src/ui/hooks/loadClient.ts
+++ src/ui/hooks/loadClient.ts
@@ -17,10 +17,6 @@ export default function loadClient(
   nameOwnerContext: NameOwner,
   userContext: User
 ): IFrontController {
-  const headers = {
-    "X-Internal-Authorization": "true",
-  };
-    
   const initialization = () => {
     let configuration: Configuration;
 
@@ -31,7 +27,6 @@ export default function loadClient(
         secretAccessKey: credentials.secretAccessKey,
         apiUrl: GPDB_API_URL,
         analyticsApiUrl: ANALYTICS_API_URL,
-        headers: headers,
       });
 
     const client = new Client(application, configuration);
