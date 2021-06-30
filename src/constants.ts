a98b3cc072c4e0587a255e19cd31b040fe1a54fc
NameCoachDevTeam <dev-team@name-coach.com>
2021-06-30 19:25:26 +0300

    Remove unneeded headers

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+/* URLs */
+export const NAMES_API_URL =
+  "https://search-gpdb-names-vgem6u5rsmbiqdrqmvx7ht2qie.us-east-1.es.amazonaws.com";
+export const GPDB_API_URL = "https://gpdb.name-coach.com/api/public/v1";
+export const ANALYTICS_API_URL = "https://analytics-api.name-coach.com/api/v1";
+
+/*
+ COLORS
+ TODO: need to put all colors in a separate js file and load it via postcss-simple-vars
+ */
+
+export const BRAND_COLOR = "#946cc1";
+export const PRIMARY_COLOR = "rgba(138, 73, 212, 0.2)";
+export const SECONDARY_COLOR = "rgba(121, 121, 121, 0.1)";
