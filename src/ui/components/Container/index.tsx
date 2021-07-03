37c389f0986bf4167f56db8cd575a1f7021b81b3
NameCoachDevTeam <dev-team@name-coach.com>
2021-07-03 12:22:29 +0300

    v1.1.2

---
+++ src/ui/components/Container/index.tsx
@@ -0,0 +1,133 @@
+import React, { useContext, useEffect, useState } from "react";
+import styles from "./styles.module.css";
+import Name, { NameTypes } from "../../../types/resources/name";
+import classNames from "classnames/bind";
+import { usePronunciations } from "../../hooks/pronunciations";
+import Loader from "../Loader";
+import FullName from "../FullName";
+import Logo from "../Logo";
+import NameLine from "../NameLine";
+import Recorder from "../Recorder";
+import ControllerContext from "../../contexts/controller";
+import useRecorderState from "../../hooks/useRecorderState";
+import AbsentName from "../AbsentName";
+
+interface Props {
+  firstName: Name;
+  lastName: Name;
+  fullName: Name;
+  verifyNames: () => PromiseLike<void>;
+  hideLogo?: boolean;
+}
+
+const cx = classNames.bind(styles);
+
+const Container = (props: Props) => {
+  const controller = useContext(ControllerContext);
+  const [loading, setLoading] = useState(false);
+  const [
+    recorderState,
+    setRecorderClosed,
+    setRecorderOpen,
+  ] = useRecorderState();
+
+  const { isOpen: isRecorderOpen } = recorderState;
+  const { firstName, lastName, fullName } = props;
+  const {
+    pronunciations,
+    setPronunciations,
+    updatePronunciationsByType,
+  } = usePronunciations();
+
+  const simpleSearch = async (type: NameTypes) => {
+    updatePronunciationsByType(
+      type,
+      await controller.simpleSearch(props[type])
+    );
+  };
+
+  const reloadName = async (type: NameTypes) => {
+    if (type === NameTypes.LastName || type === NameTypes.FirstName)
+      return await simpleSearch(type);
+    else return await props.verifyNames();
+  };
+
+  const openRecorder = (name, type) => setRecorderOpen(true, name, type);
+
+  useEffect(() => {
+    const complexSearch = async () => {
+      const existedNames = [firstName, lastName, fullName].filter(
+        (n) => n.exist
+      );
+
+      if (existedNames.length === 0) return;
+      setLoading(true);
+      setPronunciations(await controller.complexSearch(existedNames));
+      setLoading(false);
+    };
+
+    complexSearch();
+  }, [props.fullName, props.firstName, props.lastName]);
+
+  return (
+    <>
+      <div className={cx("head-line")}>
+        {!props.hideLogo && <Logo />}
+
+        <FullName
+          name={fullName.key}
+          pronunciations={pronunciations.fullName}
+          reload={reloadName}
+          onRecorderClick={openRecorder}
+        >
+          <span className={cx({ "name-word--secondary": !firstName.exist })}>
+            {`${firstName.key}, `}
+          </span>
+          <span className={cx({ "name-word--secondary": !lastName.exist })}>
+            {lastName.key}
+          </span>
+        </FullName>
+      </div>
+
+      {!pronunciations.fullName?.[0]?.nameOwnerCreated && (
+        <hr className={styles.divider} />
+      )}
+
+      {loading && <Loader />}
+
+      {isRecorderOpen && !loading && (
+        <Recorder
+          name={recorderState.name}
+          type={recorderState.type}
+          onRecorderClose={setRecorderClosed}
+        />
+      )}
+
+      {!loading &&
+        !isRecorderOpen &&
+        !pronunciations.fullName?.[0]?.nameOwnerCreated && (
+          <>
+            {[firstName, lastName].map((name, index) => (
+              <React.Fragment key={`${name.key}-${index}`}>
+                {name.exist ? (
+                  <NameLine
+                    pronunciations={pronunciations[name.type]}
+                    name={name.key}
+                    type={name.type}
+                    reload={reloadName}
+                    onRecorderClick={openRecorder}
+                  />
+                ) : (
+                  <AbsentName name={name.key} type={name.type} />
+                )}
+
+                {index === 0 && <hr className={styles.divider} />}
+              </React.Fragment>
+            ))}
+          </>
+        )}
+    </>
+  );
+};
+
+export default Container;
---
+++ src/ui/components/FullName/index.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode, useContext, useEffect, useState } from "react";
+import styles from "./styles.module.css";
+import nameLineStyles from "../NameLine/styles.module.css";
+import Pronunciation from "../../../types/resources/pronunciation";
+import Player from "../Player";
+import RecordAction from "../Actions/Record";
+import UserResponseAction from "../Actions/UserResponse";
+import { UserResponse } from "gpdb-api-client";
+import ControllerContext from "../../contexts/controller";
+import { NameTypes } from "../../../types/resources/name";
+
+interface Props {
+  children: ReactNode;
+  name: string;
+  pronunciations: Pronunciation[];
+  reload: (type: NameTypes) => void;
+  onRecorderClick: (name: string, type: NameTypes) => void;
+}
+
+const FullName = (props: Props) => {
+  const controller = useContext(ControllerContext);
+  const [pronunciation, setPronunciation] = useState<Pronunciation | null>();
+
+  const onRecord = () => props.onRecorderClick(props.name, NameTypes.FullName);
+  const onUserResponse = async () => {
+    const response =
+      pronunciation?.userResponse?.response === UserResponse.Save
+        ? UserResponse.Reject
+        : UserResponse.Save;
+
+    await controller.createUserResponse(pronunciation.id, response);
+    setTimeout(() => props.reload(NameTypes.FullName), 1500);
+  };
+
+  useEffect(() => {
+    setPronunciation(props.pronunciations?.[0]);
+  }, [props.pronunciations]);
+
+  return (
+    <div className={styles.head}>
+      <div className={styles.head__names}>{props.children}</div>
+
+      {pronunciation?.userResponse && (
+        <div className={styles.head__actions}>
+          <Player
+            audioSrc={pronunciation.audioSrc}
+            className={nameLineStyles.pronunciation__action}
+          />
+          <RecordAction
+            className={nameLineStyles.pronunciation__action}
+            onClick={onRecord}
+          />
+          <UserResponseAction
+            className={nameLineStyles.pronunciation__action}
+            onClick={onUserResponse}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FullName;
