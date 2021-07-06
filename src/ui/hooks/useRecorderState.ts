b343a2a3e8850413e78ef1c4bd861438b0dc24c4
NameCoachDevTeam <dev-team@name-coach.com>
2021-07-06 17:46:37 +0700

    [#178712531]terms and conditions opt-in prior to initial self-recording

--- src/ui/hooks/useRecorderState.ts
+++ src/ui/hooks/useRecorderState.ts
@@ -1,10 +1,17 @@
 import { useState } from "react";
 import { NameTypes } from "../../types/resources/name";
 
+export type TermsAndConditions = {
+  component: JSX.Element,
+  isAccepted(): Promise<boolean>;
+  onAccept(): Promise<void>;
+};
+
 export type RecorderState = {
   isOpen: boolean;
   name: string | null;
   type: NameTypes | null;
+  termsAndConditions: TermsAndConditions | null;
 };
 
 export type SetRecorderState = (
@@ -18,18 +25,30 @@ function useRecorderState(
     isOpen: false,
     name: null,
     type: null,
+    termsAndConditions: null,
   }
 ) {
   const [recorderState, addState] = useState<RecorderState>(defaultState);
 
-  const setRecorderOpen: SetRecorderState = (
+  const setRecorderOpen: (
     isOpen: boolean,
     name: string,
-    type: NameTypes
-  ) => addState({ name, type, isOpen });
+    type: NameTypes,
+    termsAndConditions: TermsAndConditions
+  ) => void = (
+    isOpen: boolean,
+    name: string,
+    type: NameTypes,
+    termsAndConditions: TermsAndConditions
+  ) => addState({ name, type, isOpen, termsAndConditions });
 
   const setRecorderClosed = () =>
-    addState({ name: null, type: null, isOpen: false });
+    addState({
+      name: null,
+      type: null,
+      isOpen: false,
+      termsAndConditions: null,
+    });
 
   return [recorderState, setRecorderClosed, setRecorderOpen] as const;
 }
