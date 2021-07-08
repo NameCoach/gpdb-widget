67160de9a81b06936601b7190e13fb70db91478f
NameCoachDevTeam <dev-team@name-coach.com>
2021-07-08 19:29:15 +0300

    Add name owner attribute for creation recording action

---
+++ src/types/front-controller.ts
@@ -0,0 +1,58 @@
+import Name, { NameTypes } from "./resources/name";
+import Pronunciation from "./resources/pronunciation";
+import { NameOwner, User, UserResponse } from "gpdb-api-client";
+
+export interface Meta {
+  uri?: string;
+}
+
+export interface PublicAttributes {
+  nameOwnerContext: NameOwner;
+  userContext: User;
+}
+
+export interface GpdbRequests {
+  complexSearch: (
+    names: Array<Omit<Name, "exist">>,
+    meta?: Meta
+  ) => PromiseLike<{ [t in NameTypes]: Pronunciation[] }>;
+  simpleSearch: (
+    name: Omit<Name, "exist">,
+    nameOwner?: NameOwner,
+    meta?: Meta
+  ) => PromiseLike<Pronunciation[]>;
+  createUserResponse: (id: string, type: UserResponse) => PromiseLike<void>;
+  createRecording: (
+    name: string,
+    type: NameTypes,
+    audio: string,
+    owner?: NameOwner
+  ) => PromiseLike<void>;
+  requestRecording: (name: string, type: NameTypes) => PromiseLike<void>;
+}
+
+export interface NamesServiceRequests {
+  verifyNames: (name: string) => PromiseLike<{ [t in NameTypes]: Name }>;
+}
+
+export interface AnalyticsRequests {
+  sendAnalytics: (
+    eventType: string,
+    message: string | object | boolean,
+    recordingId?: string,
+    rootUrl?: string
+  ) => PromiseLike<void>;
+}
+
+export interface SettingsRequests {
+  saveAudioSampleRate: (rate: number) => PromiseLike<void>;
+  loadAudioSampleRate: () => PromiseLike<number>;
+}
+
+type IFrontController = PublicAttributes &
+  GpdbRequests &
+  NamesServiceRequests &
+  AnalyticsRequests &
+  Partial<SettingsRequests>;
+
+export default IFrontController;
