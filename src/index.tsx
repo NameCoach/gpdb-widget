696831af089ebad8dad12c33afe1f191a59380a3
NameCoachDevTeam <dev-team@name-coach.com>
2021-07-14 15:29:18 +0300

    Update props for Recorder

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import colors from "./ui/styles/colors.css";
+
+// COMPONENTS
+import Widget from "./ui/components/Widget";
+import MyInfo from "./ui/components/MyInfo";
+import Loader from "./ui/components/Loader";
+import FullNamesList from "./ui/components/FullNamesList";
+import Recorder from "./ui/components/Recorder";
+import Player from "./ui/components/Player";
+
+// TYPES
+import type IFrontController from "./types/front-controller";
+
+// CONTEXTS
+import ControllerContext from "./ui/contexts/controller";
+
+// HOOKS
+import loadClient from "./ui/hooks/loadClient";
+
+import { Configuration } from "gpdb-api-client";
+
+export {
+  Widget,
+  MyInfo,
+  Recorder,
+  Player,
+  loadClient,
+  Configuration,
+  Loader,
+  FullNamesList,
+  ControllerContext,
+  IFrontController,
+};
