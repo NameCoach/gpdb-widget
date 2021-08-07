b286ba6e819c5a7b5558c0206b9617841b4ac2de
NameCoachDevTeam <dev-team@name-coach.com>
2021-08-07 11:39:40 +0300

    [NAM-99] Update name parser logic

--- src/core/name-parser.ts
+++ src/core/name-parser.ts
@@ -5,6 +5,8 @@ interface NPResult {
 
 export default class NameParser {
   parse(name: string): NPResult {
+    if (!name.includes(" ")) return { firstName: null, lastName: null };
+
     const parsedName = name.split(" ");
     const firstName = parsedName.slice(0, -1).join(" ").trim() || null;
     const lastName = parsedName[parsedName.length - 1] || null;
