96afc40e8266d5a0ae4de1dc3785f05165a4b452
NameCoachDevTeam <dev-team@name-coach.com>
2021-09-24 15:35:18 +0300

    NAM-288 Analytics error (staging) from beta outlook addin

--- src/core/front.controller.ts
+++ src/core/front.controller.ts
@@ -168,17 +168,18 @@ export default class FrontController implements IFrontController {
     recordingId?: string,
     rootUrl?: string
   ): Promise<void> {
-    await this.apiClient.analyticsEvents.create({
+    await this.apiClient.browserExtension.create({
       entityId: this.nameOwnerContext.signature,
       customerId: this.userContext.email || "anonymous",
       entityType: "browser_extension_user",
-      rootUrl,
+      rootUrl: rootUrl || "emptyUrl",
       eventType,
       recordingId,
       message:
         typeof message === "object" ? JSON.stringify(message) : String(message),
       userId: this.nameOwnerContext.signature,
-      toolSignature: "gpdb-widget",
+      toolSignature: "browser_extension",
+      versionInfo: {}
     });
   }
 
