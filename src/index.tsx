f58ea5235c7b5ea8aacabe7d4b98ef90a9bd1ed1
NameCoachDevTeam <dev-team@name-coach.com>
2021-10-18 23:07:03 +0700

    chore(release): 1.2.72

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+// COMPONENTS
+import Widget from "./ui/components/Widget";
+import MyInfo from "./ui/components/MyInfo";
+import Loader from "./ui/components/Loader";
+import FullNamesList from "./ui/components/FullNamesList";
+import Recorder from "./ui/components/Recorder";
+import Player from "./ui/components/Player";
+import SearchWidget from "./ui/components/SearchWidget";
+
+// TYPES
+import type IFrontController from "./types/front-controller";
+
+// INTERFACES
+import NameParser, { NPResult } from "./types/name-parser";
+
+// CONTEXTS
+import ControllerContext from "./ui/contexts/controller";
+
+// HOOKS
+import loadClient from "./ui/hooks/loadClient";
+
+import { Configuration } from "gpdb-api-client";
+
+export {
+  Widget,
+  MyInfo,
+  Recorder,
+  Player,
+  loadClient,
+  Configuration,
+  Loader,
+  FullNamesList,
+  ControllerContext,
+  IFrontController,
+  NPResult,
+  NameParser,
+  SearchWidget,
+};
