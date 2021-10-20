746272aa1d00427ee52df0944dd40776a42f48de
NameCoachDevTeam <dev-team@name-coach.com>
2021-10-20 15:51:39 +0300

    NAM-418-fix-simple-search-results-filtering

--- src/ui/components/NoPermissionsError/index.tsx
+++ src/ui/components/NoPermissionsError/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from "classnames/bind";
-import styles from "../MyInfo/styles.module.css";
+import styles from "../PronunciationMyInfoWidget/styles.module.css";
 
 const cx = classNames.bind(styles);
 
