1bc72fa883d3893c583f595a7021ba9d9e1c3669
NameCoachDevTeam <dev-team@name-coach.com>
2021-11-01 18:03:25 +0300

    NAM-393 share audio url feature

---
+++ src/constants.ts
@@ -0,0 +1,23 @@
+/* URLs */
+export const NAMES_API_URL =
+  "https://search-gpdb-names-vgem6u5rsmbiqdrqmvx7ht2qie.us-east-1.es.amazonaws.com/recordings_new";
+export const GPDB_API_URL = "https://gpdb.name-coach.com/api/public/v1";
+export const ANALYTICS_API_URL = "https://analytics-api.name-coach.com/api/v1";
+
+/*
+ COLORS
+ TODO: need to put all colors in a separate js file and load it via postcss-simple-vars
+ */
+
+export const BRAND_COLOR = "#946cc1";
+export const PRIMARY_COLOR = "rgba(138, 73, 212, 0.2)";
+export const SECONDARY_COLOR = "rgba(121, 121, 121, 0.1)";
+
+// text
+export const SEARCH_BOX_TIP =
+  "Input the name you want to find pronunciation for. You can also find the name and its pronunciation by providing an email. If you want to find a fullname, please use whitespace or coma to separate the name parts.";
+
+export const SAVE_PITCH_TIP =
+  "Pitch settings changes do not effect already recorded pronunciation.<br>Please, re-record your pronunciation to apply the new settings.";
+
+export const DEFAULT_NAME_OWNER = "outlook-app";
