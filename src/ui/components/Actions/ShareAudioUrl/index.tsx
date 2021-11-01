3419a0b2bd3ad64912298ac995bc65aa81c384d8
NameCoachDevTeam <dev-team@name-coach.com>
2021-11-01 18:03:25 +0300

    NAM-393 share audio url feature

---
+++ src/ui/components/Actions/ShareAudioUrl/index.tsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+import classNames from "classnames/bind";
+import styles from "./styles.module.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import ReactTooltip from "react-tooltip";
+
+const cx = classNames.bind(styles);
+
+interface Props {
+  url: string;
+  text: string;
+}
+
+const ShareAudioUrlAction = (props: Props): JSX.Element => {
+  const tooltipDataRef = useRef(null);
+
+  const showTooltip = (): void => {
+    ReactTooltip.show(tooltipDataRef.current);
+  };
+
+  return (
+    <>
+      <CopyToClipboard text={props.url}>
+        <div
+          ref={tooltipDataRef}
+          data-tip={props.text}
+          aria-label="Share pronunciation"
+          className={cx(styles.icon_button)}
+          onClick={showTooltip}
+        >
+          <ReactTooltip
+            uuid="share_audio_tooltip"
+            event="none"
+            eventOff="mouseout"
+            delayHide={500}
+            textColor="white"
+            backgroundColor="#946cc1"
+          />
+
+          <i className={cx("share-audio-url")} />
+        </div>
+      </CopyToClipboard>
+    </>
+  );
+};
+
+export default ShareAudioUrlAction;
