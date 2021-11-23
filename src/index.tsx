6343c252e16bb2c845bf56deb8e54bf3eabb2d90
NameCoachDevTeam <dev-team@name-coach.com>
2021-11-23 11:30:28 +0000

    Merge branch 'NAM-448-Support-in-phonetics-front-ends' into 'master'
    NAM-448 [GPDB Widget] Support for phonetics in the front ends
    
    See merge request NameCoach/sdks/gpdb-widget!55

---
+++ src/index.tsx
@@ -0,0 +1,38 @@
+// COMPONENTS
+import Widget from "./ui/components/Widget";
+import PronunciationMyInfoWidget from "./ui/components/PronunciationMyInfoWidget";
+import Loader from "./ui/components/Loader";
+import FullNamesList from "./ui/components/FullNamesList";
+import Recorder from "./ui/components/Recorder";
+import Player from "./ui/components/Player";
+import SearchWidget from "./ui/components/SearchWidget";
+
+// TYPES
+import type IFrontController from "./types/front-controller";
+
+// INTERFACES
+import NameParser, { NPResult } from "./types/name-parser";
+
+// CONTEXTS
+import ControllerContext from "./ui/contexts/controller";
+
+// HOOKS
+import loadClient from "./ui/hooks/loadClient";
+
+import { Configuration } from "gpdb-api-client";
+
+export {
+  Widget,
+  PronunciationMyInfoWidget,
+  Recorder,
+  Player,
+  loadClient,
+  Configuration,
+  Loader,
+  FullNamesList,
+  ControllerContext,
+  IFrontController,
+  NPResult,
+  NameParser,
+  SearchWidget,
+};
