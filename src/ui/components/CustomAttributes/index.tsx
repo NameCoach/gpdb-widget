55e23a12c177d0139e88d36955596ae2bf34db4a
NameCoachDevTeam <dev-team@name-coach.com>
2021-11-25 17:57:25 +0300

    NAM-435 ui changes to reduce horisontal space between elements; redesigned custom attributes

--- src/ui/components/CustomAttributes/index.tsx
+++ src/ui/components/CustomAttributes/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./styles.module.css";
 import Checkbox from "../Inputs/Checkbox";
-import InputLine from "../Inputs/InputLine";
+import DisabledInput from "../Inputs/DisabledInput";
 import CustomAttribute, {
   AttributePresentation,
 } from "../../../types/resources/custom-attribute";
+import Textarea from "../Inputs/Textarea";
 
 interface Props {
   attributes: CustomAttribute[];
@@ -12,30 +13,43 @@ interface Props {
 }
 
 const CustomAttributes = (props: Props) => (
-  <div className={styles.scrollable}>
-    {props.attributes.map((attribute) => {
-      if (attribute.presentation === AttributePresentation.Checkbox) {
-        return (
-          <Checkbox
-            key={attribute.id}
-            label={attribute.label}
-            value={attribute.value as boolean}
-            id={attribute.id}
-            disabled={props.disabled}
-          />
-        );
-      } else {
-        return (
-          <InputLine
-            key={attribute.id}
-            label={attribute.label}
-            value={attribute.value as string}
-            id={attribute.id}
-            disabled={props.disabled}
-          />
-        );
-      }
-    })}
+  <div className={styles.attributes_wrapper}>
+    <div className={styles.scrollable}>
+      {props.attributes.map((attribute) => {
+        if (attribute.presentation === AttributePresentation.Checkbox) {
+          return (
+            <Checkbox
+              key={attribute.id}
+              label={attribute.label}
+              value={attribute.value as boolean}
+              id={attribute.id}
+              disabled={props.disabled}
+            />
+          );
+        } else {
+          return (
+            <>
+              {!props.disabled && (
+                <Textarea
+                  key={attribute.id}
+                  label={attribute.label}
+                  value={attribute.value as string}
+                  id={attribute.id}
+                />
+              )}
+              {props.disabled && (
+                <DisabledInput
+                  key={attribute.id}
+                  label={attribute.label}
+                  value={attribute.value as string}
+                  id={attribute.id}
+                />
+              )}
+            </>
+          );
+        }
+      })}
+    </div>
   </div>
 );
 
