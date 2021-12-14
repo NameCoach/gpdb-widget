a565c47e77fe3024ff531b5f0350a9d5f0af6a7c
NameCoachDevTeam <dev-team@name-coach.com>
2021-12-14 14:10:05 +0300

    NAM-587 add configurable info component

--- src/ui/components/Actions/Collapsable/index.tsx
+++ src/ui/components/Actions/Collapsable/index.tsx
@@ -6,16 +6,23 @@ interface Props {
   active?: boolean;
   className?: string;
   onClick?: MouseEventHandler;
+  effectsOff?: boolean;
 }
 
 const cx = classNames.bind(styles);
 
-const CollapsableAction = (props: Props) => (
+const CollapsableAction = (props: Props): JSX.Element => (
   <div
-    className={cx(props.className, styles.wrapper, { active: props.active })}
+    className={cx(props.className, styles.wrapper, {
+      active: props.effectsOff ? false : props.active,
+    })}
     onClick={props.onClick}
   >
-    {props.active ? <i className={styles.minus_icon} /> : <i className={styles.plus_icon} />}
+    {props.active ? (
+      <i className={styles.minus_icon} />
+    ) : (
+      <i className={styles.plus_icon} />
+    )}
   </div>
 );
 
