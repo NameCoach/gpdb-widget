4c66d566552fa4ec9e5d15465f6b9d06b8ce8420
NameCoachDevTeam <dev-team@name-coach.com>
2021-12-17 11:33:49 +0300

    NAM-315 added client loader for extension; fixed refreshing state after recording creation in Container; fixed nameOwnerCreated logic

---
+++ src/ui/components/FullName/index.tsx
@@ -0,0 +1,63 @@
+import React, { ReactNode, useEffect, useMemo, useState } from "react";
+import styles from "./styles.module.css";
+import nameLineStyles from "../NameLine/styles.module.css";
+import Pronunciation from "../../../types/resources/pronunciation";
+import Player from "../Player";
+import RecordAction from "../Actions/Record";
+import { NameTypes } from "../../../types/resources/name";
+
+interface Props {
+  children: ReactNode;
+  name: string;
+  pronunciations: Pronunciation[];
+  reload: (type: NameTypes) => void;
+  onRecorderClick: (name: string, type: NameTypes) => void;
+  canPronunciationCreate: boolean;
+}
+
+const FullName = (props: Props): JSX.Element => {
+  const [pronunciation, setPronunciation] = useState<Pronunciation | null>();
+
+  const onRecord = (): void =>
+    props.onRecorderClick(props.name, NameTypes.FullName);
+
+  const canCreatePronunciation = useMemo(() => {
+    return pronunciation
+      ? !pronunciation?.isHedb
+      : props.canPronunciationCreate;
+  }, [pronunciation?.isHedb]);
+
+  useEffect(() => {
+    setPronunciation(props.pronunciations?.[0]);
+  }, [props.pronunciations]);
+
+  return (
+    <div className={styles.head}>
+      <div className={styles.head__names}>
+        <span>{props.children}</span>
+        {pronunciation?.phoneticSpelling && (
+          <div className={styles.phonetic}>
+            {pronunciation.phoneticSpelling}
+          </div>
+        )}
+      </div>
+
+      <div className={styles.head__actions}>
+        {pronunciation && pronunciation.audioSrc && (
+          <Player
+            audioSrc={pronunciation.audioSrc}
+            className={nameLineStyles.pronunciation__action}
+          />
+        )}
+        {canCreatePronunciation && (
+          <RecordAction
+            className={nameLineStyles.pronunciation__action}
+            onClick={onRecord}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default FullName;
