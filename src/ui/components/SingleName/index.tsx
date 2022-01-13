383405fbae5edcbd8da89a9e849e0cf9398fe1d1
NameCoachDevTeam <dev-team@name-coach.com>
2022-01-13 12:46:57 +0000

    Merge branch 'NAM-760-add-style-context' into 'master'
    NAM-760 Add style context
    
    Closes NAM-760
    
    See merge request NameCoach/sdks/gpdb-widget!79

---
+++ src/ui/components/SingleName/index.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Name, { NameTypes } from "../../../types/resources/name";
+import NameLine from "../NameLine";
+import AbsentName from "../AbsentName";
+
+import Pronunciation from "../../../types/resources/pronunciation";
+
+interface Props {
+  canRecordingRequestCreate: boolean;
+  canUserResponse: boolean;
+  canPronunciationCreate: boolean;
+  canRecordingRequestFind: boolean;
+  openRecorder: (name, type) => void;
+  reloadName: (type: NameTypes) => void;
+  name: Name;
+  pronunciations: Pronunciation[];
+}
+
+const SingleName = (props: Props): JSX.Element => {
+  const { name, pronunciations } = props;
+
+  return (
+    <>
+      {name.exist ? (
+        <NameLine
+          pronunciations={pronunciations}
+          name={name.key}
+          type={name.type}
+          reload={props.reloadName}
+          onRecorderClick={props.openRecorder}
+          canRecord={props.canPronunciationCreate}
+          canUserResponse={props.canUserResponse}
+        />
+      ) : (
+        <AbsentName
+          canRecordingRequestCreate={props.canRecordingRequestCreate}
+          canPronunciationCreate={props.canPronunciationCreate}
+          canRecordingRequestFind={props.canRecordingRequestFind}
+          onRecorderClick={props.openRecorder}
+          name={name.key}
+          type={name.type}
+        />
+      )}
+    </>
+  );
+};
+
+export default SingleName;
