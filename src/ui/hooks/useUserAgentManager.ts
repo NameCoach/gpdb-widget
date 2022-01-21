3142aa8de5f65d153121eeaaf4c75aee2a095d26
NameCoachDevTeam <dev-team@name-coach.com>
2022-01-21 11:02:21 +0300

    NAM-760 Refactor user agent manager and style context

--- src/ui/hooks/useUserAgentManager.ts
+++
@@ -1,6 +0,0 @@
-import IUserAgentManager from "../../types/user-agent-manager";
-import UserAgentManager from "../../core/userAgentManager";
-
-export default function useUserAgentManager(): IUserAgentManager {
-  return new UserAgentManager();
-}
