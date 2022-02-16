bc2f6f2f743a60e4090456e0f780d18bd6a2a8b7
NameCoachDevTeam <dev-team@name-coach.com>
2022-02-16 09:38:31 +0000

    Merge branch 'NAM-964-add-sentry-handler-to-player-component' into 'master'
    NAM-964 Add sentry handler to player component
    
    Closes NAM-964
    
    See merge request NameCoach/sdks/gpdb-widget!98

---
+++ src/ui/components/CustomAttributes/index.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styles from "./styles.module.css";
+import Checkbox from "../Inputs/Checkbox";
+import DisabledInput from "../Inputs/DisabledInput";
+import CustomAttribute, {
+  AttributePresentation,
+} from "../../../types/resources/custom-attribute";
+import Textarea from "../Inputs/Textarea";
+
+interface Props {
+  attributes: CustomAttribute[];
+  disabled: boolean;
+}
+
+const CustomAttributes = (props: Props): JSX.Element => (
+  <div className={styles.attributes_wrapper}>
+    <div className={styles.scrollable}>
+      {props.attributes.map((attribute) => {
+        if (attribute.presentation === AttributePresentation.Checkbox) {
+          return (
+            <Checkbox
+              key={attribute.id}
+              label={attribute.label}
+              value={attribute.value as boolean}
+              id={attribute.id}
+              disabled={props.disabled}
+            />
+          );
+        } else {
+          return (
+            <>
+              {!props.disabled && (
+                <Textarea
+                  key={attribute.id}
+                  label={attribute.label}
+                  value={attribute.value as string}
+                  id={attribute.id}
+                />
+              )}
+              {props.disabled && (
+                <DisabledInput
+                  key={attribute.id}
+                  label={attribute.label}
+                  value={attribute.value as string}
+                  id={attribute.id}
+                />
+              )}
+            </>
+          );
+        }
+      })}
+    </div>
+  </div>
+);
+
+export default CustomAttributes;
