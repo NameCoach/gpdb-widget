97579a409164b046edc80a6c9b6133bba10ac296
NameCoachDevTeam <dev-team@name-coach.com>
2022-02-24 08:18:14 +0000

    Merge branch 'NAM-603-fix-checkbox-value' into 'master'
    Resolve NAM-603 "Fix checkbox value"
    
    Closes NAM-603
    
    See merge request NameCoach/sdks/gpdb-widget!102

---
+++ src/ui/hooks/useRecorderState.ts
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { NameTypes } from "../../types/resources/name";
+
+export type TermsAndConditions = {
+  component: JSX.Element;
+  isAccepted(): Promise<boolean>;
+  onAccept(): Promise<void>;
+};
+
+export type ErrorHandler = {
+  capture(exception: any): void;
+};
+
+export type RecorderState = {
+  isOpen: boolean;
+  name: string | null;
+  type: NameTypes | null;
+  termsAndConditions: TermsAndConditions | null;
+};
+
+export type SetRecorderState = (
+  isOpen: boolean,
+  name?: string,
+  type?: NameTypes
+) => void;
+
+function useRecorderState(
+  defaultState = {
+    isOpen: false,
+    name: null,
+    type: null,
+    termsAndConditions: null,
+  }
+) {
+  const [recorderState, addState] = useState<RecorderState>(defaultState);
+
+  const setRecorderOpen: (
+    isOpen: boolean,
+    name: string,
+    type: NameTypes,
+    termsAndConditions: TermsAndConditions
+  ) => void = (
+    isOpen: boolean,
+    name: string,
+    type: NameTypes,
+    termsAndConditions: TermsAndConditions
+  ) => addState({ name, type, isOpen, termsAndConditions });
+
+  const setRecorderClosed = () =>
+    addState({
+      name: null,
+      type: null,
+      isOpen: false,
+      termsAndConditions: null,
+    });
+
+  return [recorderState, setRecorderClosed, setRecorderOpen] as const;
+}
+
+export default useRecorderState;
