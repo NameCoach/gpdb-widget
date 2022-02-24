f6e88d92b16db173ba8fac757b8d550243a6907f
NameCoachDevTeam <dev-team@name-coach.com>
2022-02-24 21:45:50 +0300

    NAM-603 improved policy logic; minor css fixes

--- src/ui/components/Recorder/machine/spec.ts
+++ src/ui/components/Recorder/machine/spec.ts
@@ -15,10 +15,11 @@ export const EVENTS = {
 const getSpec = (controller, owner): { [x: string]: any } => {
   const nameOwner = owner || controller.nameOwnerContext;
   const canCustomAttributesCreate =
+    nameOwner.signature === controller.userContext.signature &&
     controller.permissions.can(Resources.CustomAttributes, "save_values") &&
     controller.permissions.can(Resources.CustomAttributes, "retrieve_config") &&
-    controller?.customAttributes?.length > 0 &&
-    nameOwner.signature === controller.userContext.signature;
+    !controller.permissions.can(Resources.Pronunciation, "create:name_budge") &&
+    controller?.customAttributes?.length > 0;
 
   if (canCustomAttributesCreate)
     return withCustomAttrsSpec({ canCustomAttributesCreate });
