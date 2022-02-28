ef262d17354267399a16054b5e82ee902d9ac83d
NameCoachDevTeam <dev-team@name-coach.com>
2022-02-28 16:59:51 +0300

    NAM-990 display custom attributes save errors

---
+++ src/core/mappers/custom-attributes/save-values-errors.map.ts
@@ -0,0 +1,43 @@
+export type Errors = { [x: string]: string };
+type JSObject = { [x: string]: any };
+type MapperOptions = {
+  errors: JSObject;
+  config: JSObject;
+};
+
+const toErrorsString = (obj): string => {
+  const rawString = JSON.stringify(obj);
+
+  return rawString.replace(/\{|\}|\[|\]|"|.*:/gi, "");
+};
+
+const unexpectedErrorObject = (
+  additionalMessage?: string
+): { [x: string]: string } =>
+  additionalMessage
+    ? { _defaultMapperError: `Something went wrong. ${additionalMessage}` }
+    : { _defaultMapperError: `Something went wrong` };
+
+export const mapCustomAttributesErrors = ({
+  errors,
+  config,
+}: MapperOptions): Errors => {
+  const obj: JSObject = errors.custom_attributes_values[0] || {};
+  const objKeys = Object.keys(obj);
+
+  if (objKeys.length === 0) return unexpectedErrorObject();
+
+  const result = objKeys.reduce((accumulator, key) => {
+    try {
+      const keyInConfig = config.find((item) => item.id === key);
+      if (!keyInConfig) throw unexpectedErrorObject(JSON.stringify(obj));
+
+      return { ...accumulator, [key]: toErrorsString(obj[key]) };
+    } catch (e) {
+      return e;
+    }
+  }, {});
+  result._defaultMapperError = "Invalid input data";
+
+  return result;
+};
