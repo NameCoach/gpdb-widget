06698c9449513ae329835893c03b3d6b3d5b2e96
NameCoachDevTeam <dev-team@name-coach.com>
2022-03-03 20:25:39 +0500

    chore(release): 1.3.89

---
+++ src/types/resources/pronunciation.ts
@@ -0,0 +1,25 @@
+import Phonetic from "./phonetic";
+import UserResponse from "./user-response";
+import CustomAttribute from "./custom-attribute";
+
+export enum AudioSource {
+  Gpdb = "gpdb",
+  NameOwner = "name_owner",
+  NameUser = "name_user",
+}
+
+export default interface Pronunciation {
+  id: string;
+  audioSrc: string;
+  nameOwnerCreated?: boolean;
+  userCreated?: boolean;
+  gpdbCreated?: boolean;
+  audioCreator: AudioSource;
+  isHedb?: boolean;
+  language?: string;
+  phoneticSpelling?: string;
+  userResponse?: UserResponse;
+  phoneticTranscriptions?: Array<Phonetic>;
+  customAttributes?: CustomAttribute[];
+  nameBadgeLink?: string;
+}
