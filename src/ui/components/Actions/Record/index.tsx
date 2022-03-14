8390c8a038fe27de4d4574ea4e6448f37a800551
NameCoachDevTeam <dev-team@name-coach.com>
2022-03-14 18:05:09 +0300

    my info section changes

--- src/ui/components/Actions/Record/index.tsx
+++ src/ui/components/Actions/Record/index.tsx
@@ -4,18 +4,21 @@ import styles from "./styles.module.css";
 
 interface Props {
   active?: boolean;
+  rerecord?: boolean;
   className?: string;
   onClick?: MouseEventHandler;
 }
 
 const cx = classNames.bind(styles);
 
-const RecordAction = (props: Props) => (
+const RecordAction = (props: Props): JSX.Element => (
   <div
     className={cx(props.className, styles.wrapper, { active: props.active })}
     onClick={props.onClick}
   >
-    <i className={styles.mic_icon} />
+    <i
+      className={props.rerecord ? styles.mic_rerecord_icon : styles.mic_icon}
+    />
   </div>
 );
 
