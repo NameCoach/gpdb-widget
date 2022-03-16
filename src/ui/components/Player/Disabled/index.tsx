aaa581b46a0577a72444d85a8afba50d160c9025
NameCoachDevTeam <dev-team@name-coach.com>
2022-03-16 21:21:09 +0300

    NAM-1048 pronunciation not available default behavior changed - disabled icon with a tooltip added

--- src/ui/components/Player/Disabled/index.tsx
+++ src/ui/components/Player/Disabled/index.tsx
@@ -1,18 +1,32 @@
 import classNames from "classnames/bind";
 import React from "react";
 import styles from "../styles.module.css";
+import ReactTooltip from "react-tooltip";
 
 const cx = classNames.bind(styles);
 
-const DisabledPlayer = (): JSX.Element => (
-  <div
-    aria-label="Disabled player"
-    className={cx(styles.player, "test", {
-      player__disabled: true,
-    })}
-  >
-    <i className={styles.speaker} />
-  </div>
-);
+const DisabledPlayer = (): JSX.Element => {
+  const tooltipId = Date.now().toString();
+
+  return (
+    <div
+      aria-label="Disabled player"
+      className={cx(styles.player, "test", "player", "unavailable__disabled")}
+    >
+      <ReactTooltip
+        id={tooltipId}
+        textColor="white"
+        backgroundColor="#946cc1"
+        multiline
+      />
+      <i
+        className={cx(styles.speakerUnavailable, "speaker-unavailable")}
+        data-tip="Pronunciations not available"
+        data-for={tooltipId}
+        data-tip-disable={false}
+      />
+    </div>
+  );
+};
 
 export default DisabledPlayer;
