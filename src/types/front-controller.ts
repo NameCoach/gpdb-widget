b0f83edabdf06e6c1fbe567b7869d36f27d31f88
NameCoachDevTeam <dev-team@name-coach.com>
2022-03-16 10:29:14 +0000

    Merge branch 'NAM-1056-client-side-preferences' into 'master'
    NAM-1056 added client-side-preferences support; added default...
    
    Closes NAM-1056
    
    See merge request NameCoach/sdks/gpdb-widget!117

---
+++ src/types/front-controller.ts
@@ -0,0 +1,101 @@
+import Name, { NameTypes } from "./resources/name";
+import Pronunciation from "./resources/pronunciation";
+import {
+  NameOwner,
+  User,
+  UserResponse,
+  PermissionsManager,
+  ClientPreferences,
+} from "gpdb-api-client";
+import NameParser from "./name-parser";
+import { loadParams } from "gpdb-api-client/build/main/types/repositories/permissions";
+import { CustomAttributeObject } from "../core/mappers/custom-attributes.map";
+
+export interface Meta {
+  uri?: string;
+}
+
+export interface PublicAttributes {
+  nameOwnerContext: NameOwner;
+  userContext: User;
+  nameParser: NameParser;
+}
+
+export interface GpdbRequests {
+  permissions: PermissionsManager;
+  customAttributes: CustomAttributeObject[];
+  preferences: ClientPreferences;
+  complexSearch: (
+    names: Array<Omit<Name, "exist">>,
+    nameOwner?: NameOwner,
+    meta?: Meta
+  ) => PromiseLike<{ [t in NameTypes]: Pronunciation[] }>;
+  simpleSearch: (
+    name: Omit<Name, "exist">,
+    nameOwner?: NameOwner,
+    meta?: Meta
+  ) => PromiseLike<Pronunciation[]>;
+  searchBySig: (
+    nameOwner?: NameOwner
+  ) => PromiseLike<
+    [Array<Omit<Name, "exist">>, { [t in NameTypes]: Pronunciation[] }]
+  >;
+  createUserResponse: (
+    id: string,
+    type: UserResponse,
+    nameOwner?: NameOwner
+  ) => PromiseLike<void>;
+  createRecording: (
+    name: string,
+    type: NameTypes,
+    audio: string,
+    owner?: NameOwner
+  ) => PromiseLike<void>;
+  requestRecording: (
+    name: string,
+    type: NameTypes,
+    nameOwner?: NameOwner
+  ) => PromiseLike<void>;
+  findRecordingRequest: (
+    name: string,
+    type: NameTypes,
+    nameOwner?: NameOwner
+  ) => PromiseLike<boolean>;
+  loadPermissions: (rest?: loadParams) => PromiseLike<void>;
+  loadCustomAttributesConfig: () => PromiseLike<void>;
+  loadClientPreferences: (rest?: loadParams) => PromiseLike<void>;
+}
+
+export interface CustomAttributesRequests {
+  saveCustomAttributes: (
+    customAttributesValues: { [x: string]: any },
+    nameOwner?: NameOwner
+  ) => Promise<{ [x: string]: any }>;
+}
+
+export interface NamesServiceRequests {
+  verifyNames: (name: string) => PromiseLike<{ [t in NameTypes]: Name }>;
+}
+
+export interface AnalyticsRequests {
+  sendAnalytics: (
+    eventType: string,
+    message: string | object | boolean,
+    recordingId?: string,
+    rootUrl?: string
+  ) => PromiseLike<void>;
+}
+
+export interface SettingsRequests {
+  saveAudioSampleRate: (rate: number) => PromiseLike<void>;
+  loadAudioSampleRate: () => PromiseLike<number>;
+}
+
+type IFrontController = PublicAttributes &
+  GpdbRequests &
+  CustomAttributesRequests &
+  NamesServiceRequests &
+  AnalyticsRequests &
+  Partial<SettingsRequests>;
+
+export default IFrontController;
