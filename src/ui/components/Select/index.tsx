c2f84219835764f0eb6b5ee32c9312f2352d155b
NameCoachDevTeam <dev-team@name-coach.com>
2022-04-21 11:02:54 +0000

    Merge branch 'NAM-900-add-small-fixes' into 'master'
    Resolve NAM-900 "Add small fixes"
    
    Closes NAM-900
    
    See merge request NameCoach/sdks/gpdb-widget!143

---
+++ src/ui/components/Select/index.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useMemo, useState } from "react";
+import Select from "react-select";
+import { BRAND_COLOR, SECONDARY_COLOR } from "../../../constants";
+
+export interface Option {
+  value: string | number;
+  label: string;
+}
+interface Props {
+  onChange: (Option) => void;
+  options: Option[];
+  className?: string;
+  styles?: { control: object; singleValue: object };
+  value?: Option;
+}
+
+const theme = (theme) => ({
+  ...theme,
+  colors: {
+    ...theme.colors,
+    primary25: SECONDARY_COLOR,
+    primary: BRAND_COLOR,
+    neutral30: BRAND_COLOR,
+  },
+});
+
+const customStyles = (controlStyles = { control: {}, singleValue: {} }) => ({
+  control: (provided, state) => ({
+    ...provided,
+    minHeight: "30px",
+    boxShadow: null,
+    ...controlStyles.control,
+  }),
+  valueContainer: (provided, state) => {
+    const value = state.getValue()[0];
+    const labelLength = value?.label?.length || 30;
+    return {
+      ...provided,
+      minHeight: "30px",
+      height: labelLength > 30 ? "45px" : "30px",
+      padding: "0 6px",
+      color: "black",
+    };
+  },
+  singleValue: (provided, state) => ({
+    ...provided,
+    ...controlStyles.singleValue,
+  }),
+  input: (provided, state) => ({
+    ...provided,
+    margin: "0px",
+  }),
+  indicatorSeparator: (state) => ({
+    display: "none",
+  }),
+  indicatorsContainer: (provided, state) => ({
+    ...provided,
+    height: "30px",
+  }),
+  menuList: (provided) => ({
+    ...provided,
+    color: "black",
+  }),
+});
+
+const SelectComponent = (props: Props): JSX.Element => {
+  const initValue = useMemo(() => props.options[0], props.options);
+
+  const handleOnChange = (selectedValue: Option): void =>
+    props.onChange(selectedValue);
+
+  return (
+    <Select
+      defaultValue={initValue}
+      value={props.value}
+      options={props.options}
+      className={props.className}
+      onChange={handleOnChange}
+      isClearable={false}
+      isSearchable={false}
+      theme={theme}
+      styles={customStyles(props.styles)}
+    />
+  );
+};
+
+export default SelectComponent;
