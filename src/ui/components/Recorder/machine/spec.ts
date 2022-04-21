1e06519e6068341d1a7cf4003ef4f32fbe60fbef
NameCoachDevTeam <dev-team@name-coach.com>
2022-04-21 17:51:20 +0300

    NAM-1206 fix close recorder endless loader bug

--- src/ui/components/Recorder/machine/spec.ts
+++ src/ui/components/Recorder/machine/spec.ts
@@ -2,16 +2,6 @@ import defaultSpec from "./default-spec";
 import withCustomAttrsSpec from "./with-custom-attributes";
 import { Resources } from "gpdb-api-client";
 
-export const EVENTS = {
-  accept: "accept",
-  start: "start",
-  ready: "ready",
-  stop: "stop",
-  save: "save",
-  fail: "fail",
-  customAttrs: "to custom attrs",
-};
-
 const getSpec = (controller, owner): { [x: string]: any } => {
   const nameOwner = owner || controller.nameOwnerContext;
   const canCustomAttributesCreate =
