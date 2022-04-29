9e3bd01fc6adb1422b20c96bb265448dd4f6b8f9
NameCoachDevTeam <dev-team@name-coach.com>
2022-04-29 15:33:34 +0300

    NAM-1340-default-audio-presence-check

--- src/ui/components/MyInfo/index.tsx
+++ src/ui/components/MyInfo/index.tsx
@@ -141,9 +141,12 @@ const MyInfo = (props: Props): JSX.Element => {
   }, [props.name, props.controller]);
 
   const getCopyButtons = (pronunciation): CopyButton[] => {
-    const result = [{ url: pronunciation.audioSrc, text: "Audio URL" }];
+    const result = [];
 
     const share_urls = customFeatures.getMetadata("gw-share-icon")["available_urls"];
+
+    if (share_urls.includes("defaultAudio"))
+      result.push({ url: pronunciation.audioSrc, text: "Audio URL" });
 
     if (share_urls.includes("nameBadge") && pronunciation.nameBadgeLink)
       result.push({ url: pronunciation.nameBadgeLink, text: "NameBadge Link" });
