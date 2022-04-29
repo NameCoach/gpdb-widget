c17bec5f250f0cd7a4d982ce8b3b9850de416851
NameCoachDevTeam <dev-team@name-coach.com>
2022-04-29 12:37:09 +0000

    Merge branch 'NAM-1340-audio-url-presence-issue' into 'master'
    Resolve NAM-1340 "Audio url presence issue"
    
    Closes NAM-1340
    
    See merge request NameCoach/sdks/gpdb-widget!154

---
+++ src/types/resources/pronunciation.ts
@@ -0,0 +1,35 @@
+import Phonetic from "./phonetic";
+import UserResponse from "./user-response";
+import CustomAttribute from "./custom-attribute";
+
+export enum AudioSource {
+  Gpdb = "gpdb",
+  NameOwner = "name_owner",
+  NameUser = "name_user",
+}
+
+export enum RelativeSource {
+  Gpdb = "gpdb",
+  RequesterSelf = "requester_self",
+  RequesterPeer = "requester_peer",
+  TargetSelf = "target_self",
+  PeerSelf = "peer_self",
+  PeerPeer = "peer_peer",
+}
+
+export default interface Pronunciation {
+  id: string;
+  audioSrc: string;
+  nameOwnerCreated?: boolean;
+  userCreated?: boolean;
+  gpdbCreated?: boolean;
+  audioCreator: AudioSource;
+  isHedb?: boolean;
+  language?: string;
+  relativeSource?: string;
+  phoneticSpelling?: string;
+  userResponse?: UserResponse;
+  phoneticTranscriptions?: Array<Phonetic>;
+  customAttributes?: CustomAttribute[];
+  nameBadgeLink?: string;
+}
