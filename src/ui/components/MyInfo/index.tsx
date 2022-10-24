a7caff56e7c16641f9604d5104dd45622514d32a
NameCoachDevTeam <dev-team@name-coach.com>
2022-10-24 18:07:01 +0300

    INT-276 use icons and icon buttons

--- src/ui/components/MyInfo/index.tsx
+++ src/ui/components/MyInfo/index.tsx
@@ -3,7 +3,6 @@ import IFrontController from "../../../types/front-controller";
 import { NameOption } from "../FullNamesList";
 import styles from "./styles.module.css";
 import classNames from "classnames/bind";
-import Loader from "../Loader";
 import CustomAttributes from "../Outlook/CustomAttributes";
 import StyleContext from "../../contexts/style";
 import useFeaturesManager from "../../hooks/useFeaturesManager";
@@ -15,6 +14,7 @@ import IStyleContext from "../../../types/style-context";
 import CustomAttributesInspector from "../Outlook/CustomAttributesInspector";
 import { CustomAttributeObject } from "../../../core/mappers/custom-attributes.map";
 import { cloneDeep } from "lodash";
+import Actions from "./Actions";
 
 interface Props {
   name: Omit<NameOption, "key">;
@@ -31,16 +31,20 @@ const MyInfo = ({
   onCustomAttributesSaved,
 }: Props): JSX.Element => {
   if (!name?.value?.trim()) throw new Error("Name shouldn't be blank");
-  
+
   const [inEdit, setInEdit] = useState<boolean>(false);
   const controller = useContext<IFrontController>(ControllerContext);
-  const [data, setData] = useState<CustomAttributeObject[]>(cloneDeep(pronunciation?.customAttributes) || []);
+  const [data, setData] = useState<CustomAttributeObject[]>(
+    cloneDeep(pronunciation?.customAttributes) || []
+  );
   const config = cloneDeep(controller.customAttributes);
   const styleContext = useContext<IStyleContext>(StyleContext);
   const customFeatures = useCustomFeatures(controller, styleContext);
   const { t } = useTranslator(controller, styleContext);
   const { can } = useFeaturesManager(controller.permissions, customFeatures);
-  const customAttrsPresent = data?.length > 0 && data.some(e => e.value && String(e.value).length !== 0);
+  const customAttrsPresent =
+    data?.length > 0 &&
+    data.some((e) => e.value && String(e.value).length !== 0);
   const customAttrsRef = useRef<Record<string, any>>({});
   const [requestErrors, setRequestErrors] = useState([]);
   const [loading, setLoading] = useState<boolean>(false);
@@ -48,16 +52,18 @@ const MyInfo = ({
   // Probably, won't need this after https://name-coach.atlassian.net/browse/INT-241
   const customAttributesDisabled = !can(
     "editCustomAttributesForSelf",
-    pronunciation,
-    );
+    pronunciation
+  );
 
-  const resetAttributes = () => {
-    console.log({pronunciation});
-    console.log({controller});
+  const resetAttributes = (): void => {
+    console.log({ pronunciation });
+    console.log({ controller });
   };
-  
+
   useEffect(() => {
-    setData(cloneDeep(pronunciation?.customAttributes) as CustomAttributeObject[]);
+    setData(
+      cloneDeep(pronunciation?.customAttributes) as CustomAttributeObject[]
+    );
   }, [pronunciation]);
 
   const saveMyInfo = async (): Promise<void> => {
@@ -75,19 +81,21 @@ const MyInfo = ({
       setRequestErrors(res.errors.custom_attributes_values);
       setLoading(false);
       return;
-    };
-    
+    }
+
     onCustomAttributesSaved();
     setRequestErrors([]);
     setLoading(false);
     setInEdit(false);
   };
 
-  const closeEdit = () => {
+  const closeEdit = (): void => {
     setInEdit(false);
     setRequestErrors([]);
     resetAttributes();
-  }
+  };
+
+  const openEdit = (): void => setInEdit(true);
 
   return (
     <div className={cx(styles.block, styles.column)}>
@@ -98,59 +106,43 @@ const MyInfo = ({
           </span>
         </div>
 
-        <div className={styles.actions}>
-          {(() => {
-            if (loading) return (
-              <Loader inline sm />
-            );
-
-            if (inEdit) return (
-              <>
-                <button
-                  className={styles.icon_btn}
-                  onClick={closeEdit}
-                >
-                  <i className={styles.close_icon} />
-                </button>
-                <button className={styles.icon_btn} onClick={saveMyInfo}>
-                  <i className={styles.save_icon} />
-                </button>
-              </>
-            );
-            else return (
-              !customAttributesDisabled &&
-              <button
-                className={styles.icon_btn}
-                onClick={(): void => setInEdit(true)}
-              >
-                <i className={styles.edit_icon} />
-              </button>
-            );
-          })()}
-        </div>
+        <Actions
+          loading={loading}
+          inEdit={inEdit}
+          closeEdit={closeEdit}
+          saveMyInfo={saveMyInfo}
+          openEdit={openEdit}
+          customAttributesDisabled={customAttributesDisabled}
+        />
 
         {/* <div className={cx(styles.actions)}>{loading && <Loader />}</div> */}
       </div>
       <div className={cx(styles.row)}>
-        {(() =>{
-          if (inEdit) return (
-            <CustomAttributes
-              disabled={!inEdit}
-              errors={requestErrors}
-              data={data?.length > 0 ? data : config}
-              ref={customAttrsRef}
-            />
-          ); else {
-            if (customAttrsPresent) return (
-              <CustomAttributesInspector data={data} />
-            ); else return (
-              <div className={styles.tip_container}>
-                <p className={styles.tip_text}>
-                  {t("my_info_empty_tip", 'Edit "My Info" to add new information')}
-                </p>
-              </div>
+        {((): JSX.Element => {
+          if (inEdit)
+            return (
+              <CustomAttributes
+                disabled={!inEdit}
+                errors={requestErrors}
+                data={data?.length > 0 ? data : config}
+                ref={customAttrsRef}
+              />
             );
-          };
+          else {
+            if (customAttrsPresent)
+              return <CustomAttributesInspector data={data} />;
+            else
+              return (
+                <div className={styles.tip_container}>
+                  <p className={styles.tip_text}>
+                    {t(
+                      "my_info_empty_tip",
+                      'Edit "My Info" to add new information'
+                    )}
+                  </p>
+                </div>
+              );
+          }
         })()}
       </div>
     </div>
