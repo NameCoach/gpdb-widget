5d8469090675700ee1acf13b05b76de15e742c6c
NameCoachDevTeam <dev-team@name-coach.com>
2022-11-28 15:13:05 +0300

    INT-244 Refactor tooltips and popups

--- src/ui/components/Actions/Request/index.tsx
+++ src/ui/components/Actions/Request/index.tsx
@@ -1,8 +1,10 @@
 import React, { MouseEventHandler } from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.css";
-import Tooltip from "../../Tooltip";
+import Tooltip from "../../../kit/Tooltip";
 import generateTooltipId from "../../../../core/utils/generate-tooltip-id";
+import useTooltip from "../../../kit/Tooltip/hooks/useTooltip";
+import useTranslator from "../../../hooks/useTranslator";
 
 interface Props {
   disabled?: boolean;
@@ -19,19 +21,29 @@ const RequestAction = ({
   tooltipId = generateTooltipId("request_action"),
   onClick,
 }: Props): JSX.Element => {
+  const { t } = useTranslator();
+  const tooltip = useTooltip<HTMLDivElement>();
+
   return (
-    <div
-      className={cx(className, styles.wrapper, {
-        disabled: disabled,
-      })}
-      onClick={onClick}
-    >
-      <Tooltip id={tooltipId} />
-      <i
-        className={styles.request_icon}
-        data-tip="Request recording"
-        data-for={tooltipId}
-      />
+    <div>
+      <Tooltip
+        id={tooltipId}
+        opener={tooltip.opener}
+        ref={tooltip.tooltipRef}
+        rightArrow
+      >
+        {t("request_recording_action_tooltip_text")}
+      </Tooltip>
+
+      <div
+        className={cx(className, styles.wrapper, {
+          disabled: disabled,
+        })}
+        onClick={onClick}
+        ref={tooltip.openerRef}
+      >
+        <i className={styles.request_icon} />
+      </div>
     </div>
   );
 };
