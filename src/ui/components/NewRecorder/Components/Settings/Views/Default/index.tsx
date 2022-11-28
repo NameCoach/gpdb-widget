7e17a1ace3c17b578333da8b375947d697978452
NameCoachDevTeam <dev-team@name-coach.com>
2022-11-28 15:13:05 +0300

    INT-244 Refactor tooltips and popups

--- src/ui/components/NewRecorder/Components/Settings/Views/Default/index.tsx
+++ src/ui/components/NewRecorder/Components/Settings/Views/Default/index.tsx
@@ -2,7 +2,8 @@ import classNames from "classnames/bind";
 import React from "react";
 import { TOOLTIP_DELAY } from "../../../../../../../constants";
 import generateTooltipId from "../../../../../../../core/utils/generate-tooltip-id";
-import Tooltip from "../../../../../Tooltip";
+import Tooltip from "../../../../../../kit/Tooltip";
+import useTooltip from "../../../../../../kit/Tooltip/hooks/useTooltip";
 import { SettingsProps } from "../../types";
 import styles from "./styles.module.css";
 
@@ -11,12 +12,23 @@ const cx = classNames.bind(styles);
 const DefaultView = ({ active, onClick }: SettingsProps): JSX.Element => {
   const options = { onClick };
   const tooltipId = generateTooltipId("recorder_settings");
+  const tooltip = useTooltip<HTMLDivElement>();
 
   return (
-    <>
-      <div className={cx(styles.settings, { active: active })} {...options} />
-      <Tooltip id={tooltipId} delayHide={TOOLTIP_DELAY} />
-    </>
+    <div>
+      {/* TODO: Check if tooltip is needed here at all*/}
+      <Tooltip
+        id={tooltipId}
+        //  delayHide={TOOLTIP_DELAY}
+        opener={tooltip.opener}
+        ref={tooltip.tooltipRef}
+      />
+      <div
+        ref={tooltip.openerRef}
+        className={cx(styles.settings, { active: active })}
+        {...options}
+      />
+    </div>
   );
 };
 
