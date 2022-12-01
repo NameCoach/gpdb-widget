2358d8eb68fdd36ff3ffc2e032b64d13b2376d21
NameCoachDevTeam <dev-team@name-coach.com>
2022-12-01 15:22:53 +0300

    INT-353 Fix recorder player tooltip

--- src/ui/components/FullNameLine/Actions/Default/index.tsx
+++ src/ui/components/FullNameLine/Actions/Default/index.tsx
@@ -4,6 +4,10 @@ import Player from "../../../Player";
 import DisabledPlayer from "../../../Player/Disabled";
 import { Props } from "../types";
 import styles from "../../styles.module.css";
+import generateTooltipId from "../../../../../core/utils/generate-tooltip-id";
+import Tooltip from "../../../../kit/Tooltip";
+import useTooltip from "../../../../kit/Tooltip/hooks/useTooltip";
+import useSpeakerAttrs from "../../../../hooks/useSpeakerAttrs";
 
 const DefaultView = ({
   pronunciation,
@@ -12,14 +16,28 @@ const DefaultView = ({
   isRecorderOpen,
   onRecorderOpen,
 }: Props): JSX.Element => {
+  const tooltip = useTooltip<HTMLDivElement>();
+  const { speakerTip } = useSpeakerAttrs(pronunciation?.audioCreator);
+
   return (
     <div className={styles.actions}>
       {pronunciation ? (
-        <Player
-          audioSrc={pronunciation.audioSrc}
-          audioCreator={pronunciation.audioCreator}
-          autoplay={autoplay}
-        />
+        <div>
+          <Tooltip
+            opener={tooltip.opener}
+            ref={tooltip.tooltipRef}
+            rightArrow
+            id={generateTooltipId("player")}
+          >
+            {speakerTip}
+          </Tooltip>
+          <Player
+            audioSrc={pronunciation.audioSrc}
+            audioCreator={pronunciation.audioCreator}
+            autoplay={autoplay}
+            ref={tooltip.openerRef}
+          />
+        </div>
       ) : (
         <DisabledPlayer />
       )}
