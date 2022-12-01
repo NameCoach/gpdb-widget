e272c98700b257dbbc86809135bcfffe8f07ed85
NameCoachDevTeam <dev-team@name-coach.com>
2022-12-01 15:22:53 +0300

    INT-353 Fix recorder player tooltip

--- src/ui/components/NameLine/Actions/Outlook/index.tsx
+++ src/ui/components/NameLine/Actions/Outlook/index.tsx
@@ -4,6 +4,10 @@ import RecordAction from "../../../Actions/Outlook/Record";
 import Player from "../../../Actions/Outlook/Player";
 import UserResponseAction from "../../../Actions/Outlook/UserResponse";
 import { Props } from "../types";
+import useTooltip from "../../../../kit/Tooltip/hooks/useTooltip";
+import useSpeakerAttrs from "../../../../hooks/useSpeakerAttrs";
+import Tooltip from "../../../../kit/Tooltip";
+import generateTooltipId from "../../../../../core/utils/generate-tooltip-id";
 
 const OutlookView = ({
   onUserResponse,
@@ -17,14 +21,28 @@ const OutlookView = ({
   audioSrc,
   audioCreator,
 }: Props): JSX.Element => {
+  const tooltip = useTooltip<HTMLDivElement>();
+  const { speakerTip } = useSpeakerAttrs(audioCreator);
+
   return (
     <ActionsPanel>
-      <Player
-        audioSrc={audioSrc}
-        audioCreator={audioCreator}
-        autoplay={autoplay}
-        onClick={onPlay}
-      />
+      <div>
+        <Tooltip
+          opener={tooltip.opener}
+          ref={tooltip.tooltipRef}
+          rightArrow
+          id={generateTooltipId("player")}
+        >
+          {speakerTip}
+        </Tooltip>
+        <Player
+          audioSrc={audioSrc}
+          audioCreator={audioCreator}
+          autoplay={autoplay}
+          onClick={onPlay}
+          ref={tooltip.openerRef}
+        />
+      </div>
 
       {showRecordAction && (
         <RecordAction onClick={onRecordClick} rerecord={rerecord} />
