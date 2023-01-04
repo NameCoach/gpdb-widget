b9e23bc1b6befd2ec6b696dbcc76538991ec3fcb
NameCoachDevTeam <dev-team@name-coach.com>
2023-01-04 17:42:51 +0400

    feature/INT-304 add multiplecheckbox and radio for custom attributes

--- src/ui/components/PersonalBlock/index.tsx
+++ src/ui/components/PersonalBlock/index.tsx
@@ -12,7 +12,9 @@ import useRecorderState, {
 import styles from "./styles.module.css";
 
 import StyleContext from "../../contexts/style";
-import useFeaturesManager, { ShowComponents } from "../../hooks/useFeaturesManager";
+import useFeaturesManager, {
+  ShowComponents,
+} from "../../hooks/useFeaturesManager";
 import useCustomFeatures from "../../hooks/useCustomFeatures";
 import useOnRecorderClose from "../../hooks/MyInfo/useOnRecorderClose";
 import MyRecording from "../MyRecording";
@@ -126,12 +128,14 @@ const PersonalBlock = (props: Props): JSX.Element => {
               relativeSource={RelativeSource.RequesterSelf}
             />
           )}
-          <MyInfo
-            name={props.name}
-            pronunciation={pronunciation}
-            onCustomAttributesSaved={onCustomAttributesSaved}
-            loading={loading}
-          />
+          {pronunciation ? (
+            <MyInfo
+              name={props.name}
+              pronunciation={pronunciation}
+              onCustomAttributesSaved={onCustomAttributesSaved}
+              loading={loading}
+            />
+          ) : null}
         </div>
       )}
     </>
