677e8e110d46a0853be00aae54cc7c965d6cf34a
NameCoachDevTeam <dev-team@name-coach.com>
2023-01-08 14:44:18 +0300

    INT-313 Add library recording feature to personal section

---
+++ src/ui/kit/NewIcons/Speakers/SpeakerPeer.tsx
@@ -0,0 +1,4 @@
+import { ReactComponent as Icon } from "../icons/speaker-peer.svg";
+import { StyledSpeakerIcon } from "./StyledSpeakerIcon";
+
+export const SpeakerPeer = StyledSpeakerIcon(Icon);
---
+++ src/ui/kit/NewIcons/Speakers/index.ts
@@ -0,0 +1,5 @@
+export * from "./Speaker";
+export * from "./SpeakerDisabled";
+export * from "./SpeakerLibrary";
+export * from "./SpeakerOwner";
+export * from "./SpeakerPeer";
