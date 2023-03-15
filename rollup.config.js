2d6bd2e26dd06351ad58a5cfab0bbc0539a0de76
NameCoachDevTeam <dev-team@name-coach.com>
2023-03-15 22:44:28 +0300

    1.12.3
    Fix build by updating dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss";
 import postcssUrl from "postcss-url";
 import autoprefixer from "autoprefixer";
@@ -40,7 +40,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
+    typescript(),
     image(),
     svgr(),
     postcss({
