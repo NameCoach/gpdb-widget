4173ea99430f3f7f1e758e7e863365abe0756abe
NameCoachDevTeam <dev-team@name-coach.com>
2023-03-16 13:23:54 +0300

    1.12.4
    This reverts commit f94cf8879703a3f29f4ada6e5782cbb5ccb86ce2.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
+import typescript from "rollup-plugin-typescript2";
 import postcss from "rollup-plugin-postcss";
 import postcssUrl from "postcss-url";
 import autoprefixer from "autoprefixer";
@@ -40,7 +40,7 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript(),
+    typescript({ useTsconfigDeclarationDir: true }),
     image(),
     svgr(),
     postcss({
